bb9c9ddde3596ad1f63b38fbdc6d3b9fd271277e
elJamesSnz <72090281+elJamesSnz@users.noreply.github.com>
2021-02-12 15:09:05 -0600

    get y post
    Se añaden métodos de prueba get y post para generar solicitudes y respuestas al servicio

--- src/service/index.js
+++ src/service/index.js
@@ -10,10 +10,27 @@ const  { HTTP_PORT = 3000} = process.env;
 const app = express();
 const blockchain = new Blockchain();
 
+blockchain.addBlock('bloque-Express');
+
 app.use(bodyParser.json());
+
+//Para obtener los bloques minados
 app.get('/blocks', (req, res) =>{
     res.json(blockchain.blocks);
 });
+
+//Para recibir la solicit de minar
+app.post('/mine', (req, res) => {
+    const { body: { data }} = req;
+    const block = blockchain.addBlock(data);
+
+    //Tras recibir la solicitud, hace la respuesta con la siguiente información
+    res.json({
+        blocks: blockchain.blocks.length,
+        block    
+    });
+
+})
 
 //Se pone a esuchcar solicitudes
 app.listen(HTTP_PORT, ()=>{
