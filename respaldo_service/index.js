c1e051eb78482e9c07ec3e89ed09d5c29cd1eeff
elJamesSnz <72090281+elJamesSnz@users.noreply.github.com>
2021-02-15 09:32:32 -0600

    P2P
    Se agrega la posibilidad de conectar nodos con otros puertos

---
+++ respaldo_service/index.js
@@ -0,0 +1,44 @@
+//Aquí comienza sevicio http
+import express from 'express'
+import bodyParser from 'body-parser'
+
+import Blockchain from '../blockchain';
+import P2PService from './p2p';
+
+//para que escuche en un puerto por defecto
+const  { HTTP_PORT = 3000} = process.env;
+
+const app = express();
+const blockchain = new Blockchain();
+//Se crea una instancia de p2pservice y se le envía la cadena de bloques
+const p2pService = new P2PService(blockchain);
+
+blockchain.addBlock('bloque-Express');
+
+app.use(bodyParser.json());
+
+//Para obtener los bloques minados
+app.get('/blocks', (req, res) =>{
+    res.json(blockchain.blocks);
+});
+
+//Para recibir la solicit de minar
+app.post('/mine', (req, res) => {
+    const { body: { data }} = req;
+    const block = blockchain.addBlock(data);
+
+    //Tras recibir la solicitud, hace la respuesta con la siguiente información
+    res.json({
+        blocks: blockchain.blocks.length,
+        block    
+    });
+
+})
+
+//Se pone a esuchcar solicitudes
+app.listen(HTTP_PORT, ()=>{
+
+    console.log(`Servicio HTTP: ${HTTP_PORT} escuchando...`);    
+    p2pService.listen();
+
+});
